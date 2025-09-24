8f938475ec5ce13a2772db3004d529cb8e294832
zhonghaochang <152367624+zhonghaochang@users.noreply.github.com>
2025-09-24 17:42:18 +0800

    Update main.py

--- tutorial_04_lp modeling/main.py
+++ tutorial_04_lp modeling/main.py
@@ -5,6 +5,7 @@ Wyndor Production Planning Optimization Main Program
 from io_openpyxl import readDataOpenpyxl, writeDataOpenpyxl
 from io_pandas import readDataPandas, writeDataPandas
 from model import formulateModel, solveModel, getOptimalDualVariableValues
+
 DATA_PATH = "wyndor.xlsx"
 
 def main():
