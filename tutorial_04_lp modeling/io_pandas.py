b0e96d747b1e79710bc9ed60d15a4f6b0cfbeaaa
TrioSoftie <1242738291@qq.com>
2025-09-24 17:11:45 +0800

    function name

--- tutorial_04_lp modeling/io_pandas.py
+++ tutorial_04_lp modeling/io_pandas.py
@@ -1,8 +1,12 @@
+"""
+Reading from and writing to an Excel file using pandas.
+"""
+
 import pandas as pd
 import constant
 
 
-def readData() -> tuple[dict, dict, dict]:
+def readDataPandas() -> tuple[dict, dict, dict]:
     """
     Read production planning data from an Excel file using pandas.
 
@@ -18,20 +22,25 @@ def readData() -> tuple[dict, dict, dict]:
 
     data = pd.read_excel(constant.DATA_PATH, sheet_name=constant.SHEET_NAME, header=None)
 
-    productProfitCols = {'Doors': constant.INPUT_DOORS_PROFIT_COL, 'Windows': constant.INPUT_WINDOWS_PROFIT_COL}
-    productHourCols = {'Doors': constant.INPUT_DOORS_HOURS_COL, 'Windows': constant.INPUT_WINDOWS_HOURS_COL}
+    productProfitCols = {
+        'Doors': constant.INPUT_PROFIT_DOORS_COL,
+        'Windows': constant.INPUT_PROFIT_WINDOWS_COL
+    }
+    productHourCols = {
+        'Doors': constant.INPUT_HOURS_DOORS_COL,
+        'Windows': constant.INPUT_HOURS_WINDOWS_COL
+    }
 
-    # Read data from the Excel sheet into a pandas DataFrame.
     productProfits = {}
     for productName in constant.PRODUCT_NAMES:
         colIndex = productProfitCols[productName]
-        profitValue = data.at[constant.INPUT_PROFIT_START_ROW - 1, colIndex - 1]
+        profitValue = data.at[constant.INPUT_PROFIT_ROW - 1, colIndex - 1]
         productProfits[productName] = profitValue
 
     plantAvailableHours = {}
     for i, plantName in enumerate(constant.PLANT_NAMES):
         rowIndex = constant.INPUT_HOURS_AVAILABLE_START_ROW - 1 + i
-        colIndex = constant.INPUT_HOURS_AVAILABLE_START_COL - 1
+        colIndex = constant.INPUT_HOURS_AVAILABLE_COL - 1
         hours = data.at[rowIndex, colIndex]
         plantAvailableHours[plantName] = hours
 
@@ -48,7 +57,7 @@ def readData() -> tuple[dict, dict, dict]:
     return productProfits, plantProductHours, plantAvailableHours
 
 
-def writeData(soln, objVal) -> None:
+def writeDataPandas(soln, objVal) -> None:
     """
     Write the solution back to the original Excel sheet using pandas.
 
@@ -59,10 +68,8 @@ def writeData(soln, objVal) -> None:
     objVal : float
         The optimal objective function value.
     """
-    # read the entire sheet
     data = pd.read_excel(constant.DATA_PATH, sheet_name=constant.SHEET_NAME, header=None)
 
-    # Note: We construct the full variable name to look up the value in the solution dictionary.
     doorsVarName = f'{constant.MODEL_VAR_NAME_PREFIX}{constant.PRODUCT_NAMES[0]}'
     windowsVarName = f'{constant.MODEL_VAR_NAME_PREFIX}{constant.PRODUCT_NAMES[1]}'
 
@@ -74,10 +81,8 @@ def writeData(soln, objVal) -> None:
 
     rowIndex = constant.OUTPUT_ROW - 1
 
-    # Loop through the prepared dictionary and update each cell in the DataFrame.
     for col_constant, value in outputValues.items():
         colIndex = col_constant - 1
         data.loc[rowIndex, colIndex] = value
 
-    # Write DataFrame back to the Excel sheet.
     data.to_excel(constant.DATA_PATH, sheet_name=constant.SHEET_NAME, index=False, header=False)
